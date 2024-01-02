815b5bce35a370c8152d651c7dde1bee586693ab
Nicholas Vietto <74371363+nvietto@users.noreply.github.com>
2024-01-02 14:49:19 -0600

    Added data documentation

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Puns from "The Last of Us"
+#'
+#' A data set that contains strings that are from the series "The Last of Us"
+#'
+#' @format A data frame with 33 rows and 1 column:
+#' \describe{
+#'   \item{pun}{The puns from the Ellie's book}
+#'   ...
+#' }
+#' @source \url{https://thelastofus.fandom.com/wiki/No_Pun_Intended:_Volume_Too}
+"data"
