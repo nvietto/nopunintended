d9cb57bf544a051006e5b10b18b2add585154996
Nicholas Vietto <74371363+nvietto@users.noreply.github.com>
2024-01-02 13:16:37 -0600

    Initial commit

---
+++ R/Pseudocode.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+data <- read_csv("data.csv")
+
+nopunintended <- function(number = NULL)
+  {
+  if (!is.null(number) && !is.numeric(number))
+    {
+    stop("The input must be a number between 1 and 33.")
+  }
+  if (!is.null(number) && any(number < 1 | number > 33))
+    {
+    stop("The input must be a number between 1 and 33.")
+  }
+  if (is.null(number))
+    {
+    sample <- (sample(1:nrow(data), 1))
+    output <- data[sample, ]$pun
+    return(output)
+  }
+  else
+  {
+    return(data$pun[number])
+  }
+  }
+
+
+
+
