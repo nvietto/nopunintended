0079b354d60ef88d79e9d7266d2eeb1b742d282d
Nicholas Vietto <74371363+nvietto@users.noreply.github.com>
2024-02-23 13:00:15 -0600

    testing

--- tests/testthat/test-nopunintended.R
+++ tests/testthat/test-nopunintended.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("only take numerics", {
+  expect_error(nopunintended(number = "abc"))
+  expect_error(nopunintended(number = "1", "2", "3"))
+})
+
+test_that("only take numbers between 1 and 33", {
+  expect_error(nopunintended(number = 0))
+  expect_error(nopunintended(number = -1))
+  expect_error(nopunintended(number = 34))
+  expect_error(nopunintended(number = 5000))
+})
+
+test_that("numbers between 1 and 33 work", {
+  expect_no_error(nopunintended(number = NULL))
+  expect_no_error(nopunintended(number = 2))
+  expect_no_error(nopunintended(number = 22))
 })
